5abfaf377ce367fc28df00371853aa14d1ec4f72
MinJiwon <jwjwmin111@gmail.com>
2022-07-28 13:19:59 +0900

    인기상품 조회 API 호출 추가

--- src/components/PopularSection.js
+++ src/components/PopularSection.js
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import ItemImages from "./ItemImages";
 import TitleInHome from "./TitleInHome";
 import { itemsData2 } from "../utils/itemsData2";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 let title = "사람들이 많이 스크랩한 인기 상품이에요";
 
@@ -11,6 +13,20 @@ const TopWrapper = styled.div`
 `;
 
 const PopularSection = () => {
+  const [popularItems, setPopularItems] = useState([]);
+  const showPopularItems = async () => {
+    try {
+      const response = await axios.get(
+        `https://hee-backend.shop:7179/product/popularity/select`
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    showPopularItems();
+  }, []);
   return (
     <TopWrapper>
       <TitleInHome title={title} />
