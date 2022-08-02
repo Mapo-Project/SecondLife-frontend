9634e2db9acd0a2e01e6c882fc9327be8ad21b9f
MinJiwon <jwjwmin111@gmail.com>
2022-08-02 14:05:40 +0900

    style: dummy data 수정

--- src/utils/itemsData.js
+++ src/utils/itemsData.js
@@ -3,37 +3,37 @@ const imgUrl = `${process.env.PUBLIC_URL}/assets/images/itemImg/`;
 export const itemsData = [
   {
     id: 1,
-    imgUrl: `${imgUrl}dress.jpg`,
+    product_img: `${imgUrl}dress.jpg`,
     size: "M",
     price: 9000,
   },
   {
     id: 2,
-    imgUrl: `${imgUrl}pants.jpg`,
+    product_img: `${imgUrl}pants.jpg`,
     size: "L",
     price: 12000,
   },
   {
     id: 3,
-    imgUrl: `${imgUrl}neckless.jpg`,
+    product_img: `${imgUrl}neckless.jpg`,
     size: null,
     price: 5000,
   },
   {
     id: 4,
-    imgUrl: `${imgUrl}dress.jpg`,
+    product_img: `${imgUrl}dress.jpg`,
     size: "M",
     price: 9000,
   },
   {
     id: 5,
-    imgUrl: `${imgUrl}pants.jpg`,
+    product_img: `${imgUrl}pants.jpg`,
     size: "L",
     price: 12000,
   },
   {
     id: 6,
-    imgUrl: `${imgUrl}neckless.jpg`,
+    product_img: `${imgUrl}neckless.jpg`,
     price: 5000,
   },
 ];
