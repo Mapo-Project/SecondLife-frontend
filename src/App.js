ad946ee7bcce7dbf8cd922b01cecf65789860834
MinJiwon <jwjwmin111@gmail.com>
2022-08-03 15:07:48 +0900

    Sign-up 삭제

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Home from "./pages/Home";
 import GlobalStyle from "./components/GlobalStyle";
 import Login from "./pages/Login";
 import Logout from "./pages/Logout";
-import SignUp from "./pages/SignUp";
 import SimpleLogin from "./auth/SimpleLogin";
 import AdditionalUserInform from "./pages/AdditionalUserInform";
 
@@ -16,7 +15,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/logout" element={<Logout />} />
-          <Route path="/signup" element={<SignUp />} />
           <Route path="/signup/add" element={<AdditionalUserInform />} />
           <Route path="/simple" element={<SimpleLogin />} />
         </Routes>
